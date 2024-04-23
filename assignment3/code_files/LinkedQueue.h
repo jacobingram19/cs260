94fee7864922384c1f9c762860405ce89eadb3bd
jacobingram19 <145952715+jacobingram19@users.noreply.github.com>
2024-04-22 17:09:34 -0700

    Add files via upload

---
+++ assignment3/code_files/LinkedQueue.h
@@ -0,0 +1,50 @@
+#include "node.h";
+
+using std::runtime_error;
+
+class LinkedQueue {
+    private:
+        // variables
+        node *head;
+        node *tail;
+
+    public:
+        // construct linked queue
+        LinkedQueue() {
+            head = nullptr;
+            tail = nullptr;
+        }
+
+        // enqueue
+        void enqueue(int new_value) { 
+            node *temporary_ptr = new node; // creates a pointer to the new node
+            temporary_ptr -> value = new_value;
+            temporary_ptr -> next = nullptr; // the node at the end of queue must point to nullptr
+
+            if (tail == nullptr) { // if the queue is empty then the tail is going to be the same node as the head
+                head = tail = temporary_ptr;
+            } else { // if the queue is not empty then update the previous tails "next"
+                tail->next = temporary_ptr;
+                tail = temporary_ptr;
+            }
+
+        }
+
+        // dequeue
+        // need to change heads values to the next node thus dissolving the current head
+        int dequeue() {
+            if (head != nullptr){
+                if (head->next != nullptr){
+                    static int original_head_val = head->value;
+                    head->value = head->next->value;
+                    head->next = head->next->next;
+                    return original_head_val;
+                } else {
+                    head = nullptr;
+                    return 0;
+                }
+            } else {
+                return -1;
+            }
+        }
+};
