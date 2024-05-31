5c35e8b3d81fc3e1ca553961bc82aaae9f9b89e8
jacobingram19 <145952715+jacobingram19@users.noreply.github.com>
2024-05-31 09:17:35 -0700

    Add files via upload

---
+++ assignment7/code_2/node.h
@@ -0,0 +1,7 @@
+struct node {
+    int value;
+    int index; // index in linked list
+    node *left; // left in binary tree
+    node *right; // right in binary tree
+    node *next; // next in linked list
+};
